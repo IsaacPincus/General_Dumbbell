eb32b428bda5f5eb6ea85acbe1508a2e50afc6ec
isaacp <isaac.pincus@monash.edu>
2018-07-19 13:38:16 +1000

    Added basic stuff from FF_Dumbbells

---
+++ Dumbbell_Validation_Tests.f90
@@ -0,0 +1,15 @@
+Program Dumbbell_Validation_tests
+    use Random_Numbers
+    use Generate_Initial_Conditions
+    use Integrate
+    use fruit
+    implicit none
+
+    call init_fruit
+    call fruit_summary
+    call fruit_finalize
+
+    contains
+
+
+End Program
---
+++ Integrate.f90
@@ -0,0 +1,112 @@
+module Integrate
+    use Random_Numbers
+    implicit none
+
+    contains
+
+    function construct_B(Q, a, delT)
+        implicit none
+        real*8, intent(in) :: Q(3), a
+        real*8, intent(in) :: delT(3,3)
+        real*8, dimension(3,3) :: construct_B
+        real*8 :: Ql, aux, g, g_til, Ql2, Ql4, Ql6
+        real*8, save :: C43 = 4.D0/3.D0
+        real*8, save :: C83 = 8.D0/3.D0
+        real*8, save :: C143 = 14.D0/3.D0
+
+        Ql2 = Q(1)**2 + Q(2)**2 + Q(3)**2
+        Ql = sqrt(Ql2)
+        Ql4 = Ql2**2
+        Ql6 = Ql2**3
+
+        aux = a/(C43*Ql*(Ql2 + C43*a2)**3)
+        g = 1.D0-aux*(Ql6 + C143*a2*Ql4 + 8.D0*a4*Ql2)
+        g_til = -aux*(Ql6 + 2.D0*a2*Ql4 - C83*a4*Ql2)
+        construct_B = sqrt(g)*delT + (sqrt(g+g_til)-sqrt(g))*dyadic_prod(Q,Q)/Ql2
+        return
+
+    end function construct_B
+
+    function step(Q, k, dt, Q0, delT, b, a, dW)
+        implicit none
+        real*8, intent(in) :: Q(3), dt, Q0, b, a, dW(3)
+        real*8, intent(in) :: delT(3,3), k(3,3)
+        real*8 :: Ql, L, RdotB2_L, Qlength, temp_R1, temp_R2
+        real*8, dimension(3) :: F, u, RHS, Qpred, RdotB2
+        real*8, dimension(3,3) :: B1, B2, B1B1, B2B2
+        real*8, dimension(3) :: step
+
+        Ql = sqrt(Q(1)**2 + Q(2)**2 + Q(3)**2)
+        F = (Ql - Q0)/(1.0D0-(Ql-Q0)**2/b)*Q/Ql
+
+        B1 = construct_B(Q, a, delT)
+
+        B1B1 = matmul(B1,B1)
+        Qpred = Q(:) + (ten_vec_dot(k,Q) - 0.5D0*ten_vec_dot(B1B1,F))*dt &
+                  + ten_vec_dot(B1,dW)
+
+        B2 = construct_B(Qpred, a, delT)
+        B2B2 = matmul(B2,B2)
+        RHS = Q + 0.5D0*(ten_vec_dot(k,Q+Qpred) - 0.5D0*ten_vec_dot(B1B1,F))*dt &
+                + ten_vec_dot(B1,dW)
+
+        L =  sqrt(RHS(1)**2 + RHS(2)**2 + RHS(3)**2)
+        u = RHS/L
+        RdotB2 = ten_vec_dot(b*B2B2*dt/4.D0,u)
+        RdotB2_L = sqrt(RdotB2(1)**2 + RdotB2(2)**2 + RdotB2(3)**2)
+
+        Qlength = find_roots( -(2.D0*Q0+L), -b+Q0**2-RdotB2_L+2.d0*L*Q0, &
+                                RdotB2_L*Q0+L*(b-Q0**2), Q0, sqrt(b) )
+
+        step = RHS*Qlength/L
+
+    end function step
+
+    pure function find_roots(a, b, c, Q0, lim)
+        implicit none
+        real*8, intent(in) :: a, b, c, Q0, lim
+        real*8 :: find_roots
+        real*8 :: Q, R, theta, x
+        integer :: i
+
+        Q = (a**2 - 3.D0*b)/9.D0
+        R = (2.D0*a**3 - 9.D0*a*b + 27.D0*c)/54.D0
+
+        theta = acos(R/sqrt(Q**3))
+
+        do i=-1,1
+            x = -2.D0*sqrt(Q)*cos((theta + real(i)*PI*2.D0)/3.D0)-a/3.D0
+            if ((x.ge.(Q0-lim)).and.(x.le.(Q0+lim))) then
+                find_roots = x
+                EXIT
+            end if
+       end do
+
+    end function find_roots
+
+    pure function dyadic_prod(vec1,vec2)
+        implicit none
+        real*8, dimension(3), intent(in) :: vec1, vec2
+        real*8, dimension(3,3) :: dyadic_prod
+        integer :: i, j
+
+        do i=1,3
+            do j=1,3
+                dyadic_prod(i,j) = vec1(i)*vec2(j)
+            end do
+        end do
+    end function dyadic_prod
+
+    pure function ten_vec_dot(tensor, vector)
+        implicit none
+        real*8, dimension(3,3), intent(in) :: tensor
+        real*8, dimension(3), intent(in) :: vector
+        real*8, dimension(3) :: ten_vec_dot
+        integer :: i
+
+        do i=1,3
+            ten_vec_dot(i) = dot_product(tensor(i,:), vector)
+        end do
+    end function ten_vec_dot
+
+end module
